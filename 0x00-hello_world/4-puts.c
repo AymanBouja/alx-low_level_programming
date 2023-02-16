c42aefd9fb318cbb35dff7dd81bef6fc6063b006
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-16 17:04:37 +0100

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - function
+ *
+ * Description: This is the main function
+ *
+ * Return: returns 0 on success
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
