ecb1d307e2350420f562afb40661a32d3c574431
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-16 18:03:17 +0100

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - function
+ *
+ * Description: print to the standard error
+ *
+ * Return: returns 1
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
