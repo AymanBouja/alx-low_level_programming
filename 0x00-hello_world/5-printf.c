fbae3630b5b976450ba11fc4ee2119cc7ced05e8
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-16 17:35:59 +0100

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,10 @@
  *
  * Description: This is a function that uses printf.
  *
- * Return: the program returns 0
- *
+ * Return: the program returns 0 on success
  */
-int main(void)	
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art\,\n");
 	return (0);
 }
