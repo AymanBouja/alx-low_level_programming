a84d1570da577077e434efb89920eef52a75314e
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-16 20:03:48 +0100

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,8 +8,6 @@
  */
 int main(void)
 {
-	char *a = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fprintf(stderr, a);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
