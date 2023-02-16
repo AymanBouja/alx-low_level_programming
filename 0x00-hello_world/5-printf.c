568b94e7df1d0b0a8e14a8c9a284d1aaa4a612c3
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-16 17:26:48 +0100

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - function
+ *
+ * Description: This is a function that uses printf.
+ *
+ * Return: the program returns 0
+ *
+ */
+int main(void)	
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
