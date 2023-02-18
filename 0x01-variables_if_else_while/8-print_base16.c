184ff2e000070facd3f2ea82410de38d9adb24bc
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-18 01:12:27 +0100

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 30; i <= 39; i++)
+	{
+	putchar(i);
+	}
+	for (i = 61; i <= 66; i++)
+	{
+	putchar(i)
+	}
+	putchar('\n');
+	return (0);
+}
