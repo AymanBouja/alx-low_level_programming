30121e726b03684ea749e3c39fb4984723ab5ea8
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-21 10:17:45 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * alphabet - function
+ *
+ * Return: the alphabet
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
