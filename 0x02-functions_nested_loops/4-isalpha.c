586ecf5565cbd3983f63d24607b4eac99833237a
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-21 15:51:56 -0800

    task 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _islower - function
+ * _isalpha - function
  *
  * @c: character
  *
  * Description: 'c' is the parameter of the character
  *
- * Return: 1 when is lower 0 when no
+ * Return: 1 when is alpha 0 when no
  */
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 65 && c <= 122)
 	return (1);
 	else
 	return (0);
