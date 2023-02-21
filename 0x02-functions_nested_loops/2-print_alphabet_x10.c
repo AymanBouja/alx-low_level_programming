360119c298eb3fdf256fba94658852bdf88eea49
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-21 14:38:41 -0800

    Task 3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 	for (c = 'a'; c <= 'z'; c++)
@@ -19,5 +19,4 @@ void print_alphabet_x10(void)
 	}
 	_putchar('\n');
 	}
-	_putchar('\n');
 }
