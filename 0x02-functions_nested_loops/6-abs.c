38ad6e3776e7378a56a260c2d989dac5395610a3
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-21 16:06:29 -0800

    7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - function
+ *
+ * @n: int
+ *
+ * Description: 'n' is the parameter int
+ *
+ * Return:the absolute of a number
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	else
+	{
+	return (-n);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - function
+ *
+ * @n: int
+ *
+ * Description: 'n' is the parameter int
+ *
+ * Return:the absolute of a number
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	a = n % 10;
+	_putchar('0' + a);
+	return (a);
+}
