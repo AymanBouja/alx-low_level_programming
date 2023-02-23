618ba00604bbd7cf23bd0e446b5a1ff713a1d69a
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-23 12:11:40 -0800

    task 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
+void print_most_numbers(void);
+
+#endif
+
