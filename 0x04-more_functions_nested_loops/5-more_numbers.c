46a61066a5ed832519f69b31ce904bb395f40e7d
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-23 12:37:10 -0800

    Task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,15 @@
  */
 void print_numbers(void)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= 14; i++)
+	for (j = 0; j < 10; j++)
+	{
+	for (i = 0; i <= 9; i++)
 	{
 	_putchar(i + '0');
+	}
+	_putchar('\n');
 	}
 	_putchar('\n');
 }
