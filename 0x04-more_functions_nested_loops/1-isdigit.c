c0974c8cf892d2463d13a80d9da31c43255647ae
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-23 09:09:44 -0800

    Task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * _isdigit - function
+ *
+ * @c: int parameter
+ *
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
