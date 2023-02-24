2bca93c42bbeee3ee512c0ba1b4216bfb6723768
Boujarfaoui Ayman <boujarfaouiayman@gmail.com>
2023-02-23 16:29:12 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,10 @@ void main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0&& i % 5 == 0)
+	{	
+		if (i == 100)
+			printf("Buzz");
+		else if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
@@ -21,4 +23,5 @@ void main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("/n");
 }
